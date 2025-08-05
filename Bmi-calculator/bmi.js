669caf2c12925a2c21d61b117486c1cd1500b7b2
zt-logics <zahaib.tariq123@gmail.com>
2025-08-05 20:24:58 +0000

    bmi calculator

--- Bmi-calculator/bmi.js
+++ Bmi-calculator/bmi.js
@@ -1,19 +1,25 @@
-const form = document.querySelector('form');
+const form = document.querySelector('form')
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const height = document.querySelector('#height').value;
-    const weight = document.querySelector('#weight').value;
-    const results = document.querySelector('#results');
+   const height = document.querySelector('#height').value;
+   const weight = document.querySelector('#weight').value;
+   const result = document.querySelector('#results');
 
-    if (height === '' || height < 0 || isNaN(height)) {
-        results.innerHTML = `Please give a valid height ${height}`;
-    }else if (weight === '' || weight < 0 || isNaN(weight)) {
-        results.innerHTML = `Please give a valid weight ${weight}`;
-    } else {
-        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-
-        results.innerHTML = `<span>${bmi}</span>`
+   if(height === '' || height <= 0 || isNaN(height)) {
+    result.innerHTML = `Please enter a valid height`;
+   } else if(weight === '' || weight <= 0 || isNaN(weight)) {
+    result.innerHTML = `Please enter a valid weight`;
+   } else{
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    if (bmi < 18.6) {
+        result.innerHTML = `You are under weight ${bmi}`;
+    } else if(bmi === 18.6 || bmi === 24.9) {
+        result.innerHTML = `You are in normal range ${bmi}`;
+    } else if(bmi > 24.9) {
+        result.innerHTML = `You are in overweight range ${bmi}`;
     }
+    // result.innerHTML = `<span>${bmi}</span>`
+   }
 })
