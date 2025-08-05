a94a1a9acf6fb7c280ccd56ec9cd58d3debc197d
zt-logics <zahaib.tariq123@gmail.com>
2025-08-05 20:34:27 +0000

    changes

--- Bmi-calculator/bmi.js
+++ Bmi-calculator/bmi.js
@@ -15,10 +15,10 @@ form.addEventListener('submit', function(event) {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
     if (bmi < 18.6) {
         result.innerHTML = `You are under weight ${bmi}`;
-    } else if(bmi === 18.6 || bmi === 24.9) {
+    } else if(bmi >= 18.6 && bmi <= 24.9) {
         result.innerHTML = `You are in normal range ${bmi}`;
-    } else if(bmi > 24.9) {
-        result.innerHTML = `You are in overweight range ${bmi}`;
+    } else {
+        result.innerHTML = `You are overweight ${bmi}`;
     }
     // result.innerHTML = `<span>${bmi}</span>`
    }
