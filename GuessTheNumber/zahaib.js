4cba369b47616b5c53dcaca8a064cb955ffdd022
zt-logics <zahaib.tariq123@gmail.com>
2025-08-10 20:09:30 +0000

    done

--- GuessTheNumber/zahaib.js
+++ GuessTheNumber/zahaib.js
@@ -1,13 +1,13 @@
 let randomNumber = parseInt(Math.random() * 100 + 1);
 
-const submit = document.querySelector('#subt');
-const userInput = document.querySelector('#guessField');
-const guessSlot = document.querySelector('.guesses');
-const remaining = document.querySelector('.lastResult');
-const lowOrHi = document.querySelector('.lowOrHi');
-const startOver = document.querySelector('.resultParas');
+const submit = document.querySelector("#subt");
+const userInput = document.querySelector("#guessField");
+const guessSlot = document.querySelector(".guesses");
+const remaining = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+const startOver = document.querySelector(".resultParas");
 
-const p = document.createElement('p');
+const p = document.createElement("p");
 
 let prevGuess = [];
 let numGuess = 1;
@@ -15,7 +15,7 @@ let numGuess = 1;
 let playGame = true;
 
 if (playGame) {
-  submit.addEventListener('click', function (e) {
+  submit.addEventListener("click", function (e) {
     e.preventDefault();
     const guess = parseInt(userInput.value);
     console.log(guess);
@@ -25,11 +25,11 @@ if (playGame) {
 
 function validateGuess(guess) {
   if (isNaN(guess)) {
-    alert('PLease enter a valid number');
+    alert("PLease enter a valid number");
   } else if (guess < 1) {
-    alert('PLease enter a number more than 1');
+    alert("PLease enter a number more than 1");
   } else if (guess > 100) {
-    alert('PLease enter a  number less than 100');
+    alert("PLease enter a  number less than 100");
   } else {
     prevGuess.push(guess);
     if (numGuess === 11) {
@@ -55,7 +55,7 @@ function checkGuess(guess) {
 }
 
 function displayGuess(guess) {
-  userInput.value = '';
+  userInput.value = "";
   guessSlot.innerHTML += `${guess}, `;
   numGuess++;
   remaining.innerHTML = `${11 - numGuess} `;
@@ -65,11 +65,27 @@ function displayMessage(message) {
   lowOrHi.innerHTML = `<h2>${message}</h2>`;
 }
 
-function newGame(){
-    //
+function newGame() {
+  const newGameButton = document.querySelector("#newGame");
+  newGameButton.addEventListener("click", function (e) {
+    randomNumber = parseInt(Math.random() * 100 + 1);
+    prevGuess = [];
+    numGuess = 1;
+    guessSlot.innerHTML = "";
+    remaining.innerHTML = `${11 - numGuess} `;
+    userInput.removeAttribute("disabled");
+    startOver.removeChild(p);
+
+    playGame = true;
+  });
 }
 
-
-function endGame(){
-    //
+function endGame() {
+  userInput.value = "";
+  userInput.setAttribute("disabled", "");
+  p.classList.add("button");
+  p.innerHTML = `<h2 id="newGame">Start new Game</h2>`;
+  startOver.appendChild(p);
+  playGame = false;
+  newGame();
 }
